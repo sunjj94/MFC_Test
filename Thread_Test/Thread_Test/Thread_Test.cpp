60168dc85308bd5fe26e967cb49636bc60ef2121
sunjj <jingjing.sun@ganx.com.cn>
2017-07-03 14:42:23 +0800

    线程同步

--- Thread_Test/Thread_Test/Thread_Test.cpp
+++ Thread_Test/Thread_Test/Thread_Test.cpp
@@ -7,6 +7,9 @@
 
 #define NAME_LINE 40
 //在线程中传入参数
+//线程同步
+
+//定义线程函数传入参数的结构体
 typedef struct __THREAD_DATA
 {
 	int nMaxNum;
@@ -18,20 +21,29 @@ typedef struct __THREAD_DATA
 	}
 }THREAD_DATA;
 
+HANDLE g_hMutex = NULL; //互斥量
+
+//线程函数
 DWORD WINAPI ThreadProc(LPVOID lpParameter)
 {
 	THREAD_DATA *pThreadData = (THREAD_DATA*)lpParameter;
 
 	for (int i = 0; i < pThreadData->nMaxNum; i++)
 	{
+		//请求获得一个互斥量锁
+		WaitForSingleObject(g_hMutex, INFINITE);//无限等待
 		printf("%s---%3d\n", pThreadData->strThreadName, i);
 		Sleep(100);
+		//释放互斥量锁
+		ReleaseMutex(g_hMutex);
 	}
 	return 0;
 }
 
 int main()
 {
+	g_hMutex = CreateMutex(NULL, FALSE, NULL);
+
 	THREAD_DATA threadData1, threadData2;
 	threadData1.nMaxNum = 5;
 	strcpy_s(threadData1.strThreadName, "线程1");
@@ -45,8 +57,12 @@ int main()
 
 	for (int i = 0; i < 5; i++)
 	{
+		//请求获得一个互斥量锁
+		WaitForSingleObject(g_hMutex, INFINITE);
 		printf("主线程：%3d\n\n", i);
 		Sleep(100);
+		//释放互斥量锁
+		ReleaseMutex(g_hMutex);
 	}
 
 	system("pause");
